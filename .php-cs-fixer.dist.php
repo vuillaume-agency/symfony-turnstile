57f9bcabe886055d1faf860a76bbb3978d1e63fe
ArnaudV <arnaud@vuillaume-agency.com>
2026-01-07 22:40:12 +0100

    build: Upgrade dev dependencies
    - Replace symplify/easy-coding-standard with php-cs-fixer 3.65
    - Upgrade phpstan to 2.1 and phpstan-symfony to 2.0
    - Upgrade phpunit to 12.0
    - Upgrade rector to 2.0
    - Update configuration files for new tool versions

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,29 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'declare_strict_types' => true,
+        'strict_param' => true,
+        'native_function_invocation' => [
+            'include' => ['@compiler_optimized'],
+            'scope' => 'namespaced',
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => false,
+            'import_functions' => false,
+        ],
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder);
